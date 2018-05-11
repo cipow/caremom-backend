b970fb0ce53a5828245a61f23526bf2475c995eb
cipow <cipoghost@gmail.com>
2018-05-11 11:56:42 +0700

    adding protected table

--- app/HospitalImage.php
+++ app/HospitalImage.php
@@ -4,6 +4,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class HospitalImage extends Model {
+  protected $table = 'hospital_images';
 
   public function hospital() {
     return $this->belongsTo('App\Hospital', 'hospital_id', 'id');
