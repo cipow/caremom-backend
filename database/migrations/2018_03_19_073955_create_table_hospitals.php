17df049531f4ae613b5e567a521f7413eaab99d7
cipow <cipoghost@gmail.com>
2018-05-14 14:45:33 +0700

    database changed

--- database/migrations/2018_03_19_073955_create_table_hospitals.php
+++ database/migrations/2018_03_19_073955_create_table_hospitals.php
@@ -22,8 +22,8 @@ class CreateTableHospitals extends Migration
             $table->string('city', 20);
             $table->string('telephone', 20);
             $table->text('logo')->nullable();
-            $table->text('latitude')->nullable();
-            $table->text('longitude')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
             $table->timestamps();
         });
     }
